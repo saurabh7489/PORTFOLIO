687b4287a119a02d5e11a710f321f3caacfb44ec
saurabh7489 <saurabhpandey7489@gmail.com>
2026-06-19 23:38:37 +0530

    initial commit

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,10 +1,12 @@
  import Header from './Header'
  import React from 'react'
  import './App.css'
+ import Home from './Home'
  function App() {
    return (
      <div>
       <Header/>
+      <Home/>
      </div>
    )
  }
